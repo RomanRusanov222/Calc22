b978331f2b8368a4cc9c67fc9c0700b08169d462
RomanRusanov222 <114281470+RomanRusanov222@users.noreply.github.com>
2023-07-12 22:03:36 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+         Scanner scanner = new Scanner(System.in);
+
+
+        try {
+            System.out.print("Введите цисло 1: ");
+            double a = scanner.nextDouble();
+
+            System.out.print("Введите число 2: ");
+            int b = scanner.nextInt();
+            PowerCalculator powerCalculator = new PowerCalculator();
+
+
+            System.out.println("resultat: " + powerCalculator.calculatePower(a,b));
+        } catch (InvalidInputException e) {
+            System.out.println("ochibka: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("ochibca vvoda: введены некорректные числа");
+        }
+
+        scanner.close();
     }
+    }
+
+    
 }
